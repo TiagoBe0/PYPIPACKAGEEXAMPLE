9267973c0e96c46403e514c03b1e1f292f8d1d7c
TiagoBe0 <santiagobergamin@gmail.com>
2025-06-25 11:32:45 -0300

    pypi deploy

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ setup(
 
 
 
-    name='pypipackageexample',
-    version='0.1.0',
-    author='SantiBe',
+    name='pypipackageexamplesbs',
+    version='0.1.3',
+    author='TiagodBe',
     license='MIT',
     description='A simple example package for PyPI',
     packages=find_packages(),                                                                                                               
-    install_requires=[
-        'numpy','math']
-        author_email='santiagobergamin@gmail.com',
+    install_requires=['numpy','math'],
+    author_email='santiagobergamin@gmail.com',
     url='https://github.com/TiagoBe0/PYPIPACKAGEEXAMPLE.git'
 
 )
